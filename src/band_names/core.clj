0fe20a596889aaa91065f8324a6cf02fba2c279e
Andrew Stine <stine.drew@gmail.com>
2013-03-19 18:22:48 -0400

    Probability added to word choice; better misspelling algorithm; leetspeak algorithm

--- src/band_names/core.clj
+++ src/band_names/core.clj
@@ -9,18 +9,23 @@
 (defn slurp-lines [file]
   (split-lines (slurp file)))
 
+(defn slurp-weighted-lines [file]
+  (map #(let [[n s] (split % #"\s" 2)]
+          [s (read-string n)])
+       (slurp-lines file)))
+
 (def band-names (sort (distinct (rest (slurp-lines "resources/band_names.txt")))))
 (def band-names-set (set band-names))
 
-(def nouns (map #(let [[n s] (split % #"\s" 2)] [s (read-string n)]) (slurp-lines "resources/nouns/nouns.txt")))
+(def nouns (slurp-weighted-lines "resources/nouns/nouns.txt"))
 
-(def adjectives (slurp-lines "resources/adjectives/28K adjectives.txt"))
+(def adjectives (slurp-weighted-lines "resources/adjectives/adjectives.txt"))
 
-(def verbs (slurp-lines "resources/verbs/31K verbs.txt"))
+(def verbs (slurp-weighted-lines "resources/verbs/verbs.txt"))
 
-(def adverbs (slurp-lines "resources/adverbs/6K adverbs.txt"))
+(def adverbs (slurp-weighted-lines "resources/adverbs/adverbs.txt"))
 
-(def prepositions (map #(let [[n s] (split % #"\s" 2)] [s (read-string n)]) (slurp-lines "resources/prepositions/prepositions.txt")))
+(def prepositions (slurp-weighted-lines "resources/prepositions/prepositions.txt"))
 
 (def male-names (slurp-lines "resources/names/male-first-names.txt"))
 (def female-names (slurp-lines "resources/names/female-first-names.txt"))
@@ -36,6 +41,12 @@
         forms (map second forms)]
     `(case (rand-nth-weighted ~(vec (map vector (iterate inc 0) weights)))
        ~@(mapcat list (iterate inc 0) forms))))
+
+(defn capitalize-words [words]
+  (join " " (map capitalize (split words #"\s+"))))
+
+(defn capitalized? [word]
+  (boolean (re-seq #"^[A-Z]" word)))
 
 (def syllable-count-distribution
   [[0 1]
@@ -58,23 +69,25 @@
 (defn merge-band-name [{:keys [base-word adjectives
                                verb adverbs
                                preposition article?
-                               plural? band?]}]
+                               plural? band?]
+                        :as band-name}]
   (join " " 
-        (map capitalize
-             (remove nil?
-                     (concat (when article?
-                               (if plural?
-                                 [(rand-do 2 "The" 1 "A")]
-                                 ["The"]))
-                             adverbs
-                             [verb]
-                             adjectives
-                             [(if plural? 
-                                (plural base-word)
-                                (singular base-word))]
-                             [preposition]
-                             (if band?
-                               ["Band"]))))))
+        (remove nil?
+                (concat (if band?
+                          ["The"]
+                          (when article?
+                            (if plural?
+                              [(rand-do 2 "The" 1 "A")]
+                              ["The"])))
+                        (map capitalize adverbs)
+                        [(when verb (capitalize verb))]
+                        (map capitalize adjectives)
+                        [(capitalize-words (if (and plural? (not preposition))
+                                       (plural base-word)
+                                       (singular base-word)))]
+                        [(when preposition (plural preposition))]
+                        (if band?
+                          ["Band"])))))
 
 (defn generate-preposition []
   (str (rand-nth-weighted prepositions)
@@ -89,17 +102,19 @@
         band-name {:base-word (rand-nth-weighted nouns)}]
     (loop [band-name band-name]
       (if (> syllables (band-name-syllables band-name))
-        (rand-do 1 (recur (assoc band-name :adjectives 
-                                 (cons (rand-nth adjectives) (:adjectives band-name))))
-                 1 (if (:verb band-name)
+        (rand-do 2 (recur (assoc band-name :adjectives 
+                                 (cons (rand-nth-weighted adjectives)
+                                       (:adjectives band-name))))
+                 2 (if (:verb band-name)
                      (recur (assoc band-name :adverbs
-                                   (cons (rand-nth adverbs) (:adverbs band-name))))
-                     (recur (assoc band-name :verb (rand-nth verbs))))
-                 1 (recur (assoc band-name :preposition (generate-preposition)))
-                 1 (if (:article? band-name)
+                                   (cons (rand-nth-weighted adverbs)
+                                         (:adverbs band-name))))
+                     (recur (assoc band-name :verb (rand-nth-weighted verbs))))
+                 2 (recur (assoc band-name :preposition (generate-preposition)))
+                 2 (if (:article? band-name)
                      (recur band-name)
                      (recur (assoc band-name :article? true)))
-                 1 (if (:plural? band-name)
+                 2 (if (:plural? band-name)
                      (recur band-name)
                      (recur (assoc band-name :plural? true)))
                  1 (if (:band? band-name)
@@ -115,6 +130,25 @@
        " "
        (capitalize (rand-nth last-names))))
 
+(defn musician-band-name []
+  (let [syllables (rand-nth-weighted syllable-count-distribution)
+        band-name {:base-word (musician-name)}]
+    (loop [band-name band-name]
+      (if (> syllables (band-name-syllables band-name))
+        (rand-do 0.5 (recur (assoc band-name :adjectives 
+                                 (cons (rand-nth-weighted adjectives)
+                                       (:adjectives band-name))))
+                 1 (if (:verb band-name)
+                     (recur (assoc band-name :adverbs
+                                   (cons (rand-nth-weighted adverbs)
+                                         (:adverbs band-name))))
+                     (recur (assoc band-name :verb (rand-nth-weighted verbs))))
+                 2 (recur (assoc band-name :preposition (generate-preposition)))
+                 5 (if (:band? band-name)
+                     (recur band-name)
+                     (recur (assoc band-name :band? true))))
+        (merge-band-name band-name)))))
+
 (defn rapper-name []
   (join " "
         (map capitalize
@@ -127,42 +161,74 @@
                        1 nil)
                       (rand-nth rap-last-names)]))))
 
-(def misspellings [[#"(ck|c)" "k"]
-                   ["in" "n"]
-                   ["in" "'n"]
-                   ["own" "one"]
-                   ["one" "own"]
-                   ["ea" "ee"]
-                   ["ti" "sh"]
-                   [#"\s(and)\s" "n"]
-                   ["s" "z"]
-                   ["y " "i "]
-                   [#"y$" "i"]
-                   ["i" "y"]
-                   ["r" "rr"]
-                   ["g" "gg"]
-                   ["through" "thru"]
-                   ["ew" "u"]
-                   ["oo" "ew"]
-                   ["le" "al"]
-                   ["ea" "ee"]
-                   ["qu" "kw"]
-                   ["qu" "qw"]
-                   ["ate" "8"]
-                   ["great" "gr8"]
-                   ["ks" "x"]
-                   ["x" "ks"]
-                   ["x" "xxx"]
-                   ["you" "u"]
-                   ["your" "yur"]
-                   ["wh" "w"]
-                   ["what" "wut"]])
+(def misspellings [[#"(ck|c|C|Ck)" "k"]
+                   [#"[Ii]n" "n"]
+                   [#"[Ii]n" "'n"]
+                   [#"(.)own" "$1one"]
+                   [#"(.)one" "$1own"]
+                   [#"[Ee]a" "ee"]
+                   [#"(.)[Tt]i" "$1sh"]
+                   [#"^and$" "n"]
+                   [#"[Ss]" "z"]
+                   [#"[Yy] " "i "]
+                   [#"[Yy]$" "i"]
+                   [#"[Ii]" "y"]
+                   [#"[Rr]" "rr"]
+                   [#"(.)g" "$1gg"]
+                   [#"[Tt]hrough" "thru"]
+                   [#"[Ee]w" "u"]
+                   [#"[Oo]o" "ew"]
+                   [#"[Ll]e$" "al"]
+                   [#"[Uu]e$" "u"]
+                   [#"[Ee]a" "ee"]
+                   [#"[Qq]u" "kw"]
+                   [#"[Qq]u" "qw"]
+                   [#"[Aa]te" "8"]
+                   [#"[Gg]reat" "gr8"]
+                   [#"[Kk]s" "x"]
+                   [#"[Xx]" "ks"]
+                   [#"[Xx]" "xxx"]
+                   [#"[Yy]ou" "U"]
+                   [#"[Yy]our" "Yur"]
+                   [#"[Ww]h" "W"]
+                   [#"[Ww]hat" "Wut"]])
 
-(defn misspell [phrase] ;TODO make these case independent
+(def leetz [[#"[Ee]" "3"]
+            [#"[Aa]" "@"]
+            [#"[Aa]" "4"]
+            [#"[Oo]" "0"]
+            [#"[Tt]" "7"]
+            [#"[Gg]" "9"]
+            [#"[Ii]" "1"]
+            [#"[Hh]" "|-|"]
+            [#"[Kk]" "|<"]
+            [#"[Kk]" "|("]
+            [#"[Yy]" "`/"]
+            [#"[Vv]" "\\\\/"]
+            [#"[Ww]" "\\\\/\\\\/"]
+            [#"[Dd]" "c|"]
+            [#"[Mm]" "/\\\\/\\\\"]
+            [#"[Mm]" "|\\\\/|"]
+            [#"[Nn]" "|\\\\|"]])
+
+(defn misspell [phrase & [thesaurus intensity]] 
+  (let [thesaurus (or thesaurus misspellings)
+        intensity (or intensity (rand))]
+    (join " "
+          (for [word (split phrase #"\s+")]
+            (let [new-word (reduce #(apply clojure.string/replace (cons %1 %2))
+                                   (lower-case word)
+                                   (take (* intensity (count thesaurus))
+                                         (shuffle thesaurus)))]
+              (if (capitalized? word)
+                (capitalize new-word)
+                (lower-case new-word)))))))
+          
   (reduce #(apply clojure.string/replace (cons %1 %2))
           phrase
-          (distinct (repeatedly (inc (rand-int (dec (count misspellings))))
-                                #(rand-nth misspellings)))))
+          (shuffle (distinct (repeatedly (inc (rand-int (dec (count misspellings))))
+                                #(rand-nth misspellings))))))
+
 
 (defn band-name-exists? [band-name]
   (boolean (band-names-set band-name)))
@@ -175,17 +241,26 @@
 (defn generate-band-name []
   (or (band-name-available?
        (rand-do 9 (basic-band-name)
-                5 (misspell (basic-band-name))
-                10 (musician-name)
+                9 (misspell (basic-band-name) misspellings 0.2)
+                1 (misspell (basic-band-name) misspellings 0.8)
+                0.3 (misspell (basic-band-name) leetz 0.7)
+                9 (musician-name)
+                7 (musician-band-name)
                 3 (rapper-name)))
       (generate-band-name)))
 
 
 (defn quiz-name []
   (let [real-name (rand-nth band-names)
-        false-name (generate-band-name)]
-    (print (str "Which band is real? \n"))
+        false-name (generate-band-name)
+        names (shuffle (list real-name false-name))]
+    (println (str "Which band is real?"))
     (doseq [[index name]
-            (map list [1 2] (shuffle (list real-name false-name)))]
-      (print (str index " " name "\n")))))
+            (map list [1 2] names)]
+      (println (str index " " name)))
+    (flush)
+    (if (= (nth names (dec (read-string (read-line))))
+           real-name)
+      (println "Correct!")
+      (println "False!"))))
 
