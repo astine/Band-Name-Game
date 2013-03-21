3135b527d7dd2f92f498dade350f935ca07c4ed5
Andrew Stine <illuminati1113@gmail.com>
2013-03-20 22:28:18 -0400

    adding webpage templates

--- project.clj
+++ project.clj
@@ -6,5 +6,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [roul "0.2.0"]
+                 [enlive "1.1.1"]
                  [incanter "1.4.1"]
+                 [compojure "1.1.5"]
                  [inflections "0.8.0"]])
