5b8c932bf01ac4e3ff4cdc979ab8f48f45b471c5
Andrew Stine <illuminati1113@gmail.com>
2013-03-25 19:28:34 -0400

    Start and end pages added

--- src/band_names/server.clj
+++ src/band_names/server.clj
@@ -11,33 +11,46 @@
             [compojure.route :as route]))
 
 (def index (html-resource "www/index.html"))
+(def game (html-resource "www/game.html"))
+(def results (html-resource "www/score.html"))
+
+(defn score-template [count-right count]
+  (let [score (ceil (* 100 (/ count-right count)))]
+    (emit* (at results [:#results]
+               (content (list {:tag :p :content (cond (< 90 score) ["Congratulations!"]
+                                                      (<= 50 score 90) ["Good job!"]
+                                                      :else [""])}
+                              {:tag :p :content `("Score:" {:tag :em :content [~score]} "%")}))))))
 
 (defn band-name-template [count-right count-wrong count band-names]
-    (emit* (at index [:.band]
-               (clone-for [[color [band validity]] band-names]
-                          (do-> (add-class color)
-                                (content {:tag :p :content band})
-                                (set-attr :href (str "/answer?choice=" color
-                                                     "&count=" count))))
-               [:#counter]
-               (content (list (str "Right: " count-right)
-                              {:tag :br}
-                              (str "Wrong: " count-wrong))))))
+  (emit* (at game [:.band]
+             (clone-for [[color [band validity]] band-names]
+                        (do-> (add-class color)
+                              (content {:tag :p :content band})
+                              (set-attr :href (str "/answer?choice=" color
+                                                   "&count=" count))))
+             [:#counter]
+             (content (list (str "Right: " count-right)
+                            {:tag :br}
+                            (str "Wrong: " count-wrong))))))
 
 (defn band-name-create [{session :session}]
-  (let [band-names (zipmap ["negative" "positive"]
-                           (shuffle [[(generate-band-name) false]
-                                     [(select-band-name) true]]))]
-    (-> (response (band-name-template (or (:count-right session) 0)
-                                      (or (:count-wrong session) 0)
-                                      (inc (or (:count session) 0))
-                                      band-names))
-        (assoc :session (assoc session :band-names band-names)))))
+  (if (and session (:count session) (> (:count session) 10))
+    (redirect (str "/score?count-right=" (:count-right session) 
+                   "&count=" (:count session)))
+    (let [band-names (zipmap ["negative" "positive"]
+                             (shuffle [[(generate-band-name) false]
+                                       [(select-band-name) true]]))]
+      (-> (response (band-name-template (or (:count-right session) 0)
+                                        (or (:count-wrong session) 0)
+                                        (inc (or (:count session) 0))
+                                        band-names))
+          (assoc :session (assoc session :band-names band-names))))))
 
 (defn band-name-pick [choice count {session :session}]
   (if (> (read-string count) (or (:count session) 0))
     (let [correct? (second ((:band-names session) choice))]
-      (-> (redirect "/")
+      (-> (redirect "/game")
           (assoc :session (assoc session
                             :count (read-string count)
                             :count-right (if correct? 
@@ -52,8 +65,14 @@
                                   (:band-names session)))))
 
 (defroutes app-routes
-  (GET "/" [:as req] (band-name-create req))
+  (GET "/" [] (-> (response (emit* (at index [:#start-link] 
+                                       (content {:tag :p :content "Start Game"}))))
+                  (assoc :session {})))
+  (GET "/game" [:as req] (band-name-create req))
   (GET "/answer" [choice count :as req] (band-name-pick choice count req))
+  (GET "/restart" [:as req] (band-name-create (assoc req :session {})))
+  (GET "/score" [count-right count] (response (score-template (read-string count-right) 
+                                                              (read-string count))))
   (route/resources "/"))
 
 (def app
